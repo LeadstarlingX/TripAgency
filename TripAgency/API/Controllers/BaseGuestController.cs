2d1790844864f50fb5396680143ada0921bc68a6
AlaaZreekie <alaasama282@gmail.com>
2025-05-26 18:00:48 +0300

    addGuest&AuthControllers

---
+++ TripAgency/API/Controllers/BaseGuestController.cs
@@ -0,0 +1,14 @@
+﻿using Application.IApplicationServices.Authentication;
+using Application.Serializer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class BaseGuestController(IAuthenticationService authenticationService, IJsonFieldsSerializer jsonFieldsSerializer) : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService = authenticationService;
+        private readonly IJsonFieldsSerializer _jsonFieldsSerializer = jsonFieldsSerializer;
+    }
+}
